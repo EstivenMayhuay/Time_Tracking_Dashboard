15f519e3208d71be79bbdc1aa855736fb541edb3
Estiven Mayhuay <developerwebhairton@gmail.com>
2021-09-27 11:41:32 -0500

    add create a file app.js, define the methods for render, obtain data

---
+++ script/app.js
@@ -0,0 +1,46 @@
+const log = console.log;
+
+$items = Array.from(document.querySelectorAll('.items'));
+$btnTime = Array.from(document.querySelectorAll('.btnTime'));
+
+async function obtainData () {
+  const response = await fetch('http://127.0.0.1:5500/data.json'),
+        json = await response.json();
+
+  return json;
+}
+
+function renderTime (arrTime) {
+  let timesCurr = Array.from(document.querySelectorAll('.timeCurrent')),
+      timesPrev = Array.from(document.querySelectorAll('.timePrevious'));
+
+  timesCurr.forEach( (timeCurr, index) => {
+    timeCurr.innerHTML = `${arrTime[index].current}hrs`;
+  });
+
+  timesPrev.forEach( (timePrev, index) => {
+    timePrev.innerHTML = `Last Week - ${arrTime[index].previous}hrs`;
+  });
+
+}
+
+async function changeTime (e) {
+  let dataTimeBtn = e.target.getAttribute('data-time'),
+      objJSON = await obtainData(),
+      objectTime = [];
+
+  objJSON.forEach( obj => {
+    if(dataTimeBtn == "daily") objectTime.push(obj.timeframes.daily);
+    else if(dataTimeBtn == "weekly") objectTime.push(obj.timeframes.weekly);
+    else if(dataTimeBtn == "monthly") objectTime.push(obj.timeframes.monthly);
+  });
+
+  log(objectTime);
+
+  // show int the DOM The time
+  renderTime(objectTime);
+}
+
+$btnTime.forEach(btn => {
+  btn.addEventListener('click', changeTime);
+});
