0fe627e3ca5772983cfcba739ae1a879d36f137d
Estiven Mayhuay <developerwebhairton@gmail.com>
2022-02-19 18:40:13 -0500

    second time to fix the url

--- script/app.js
+++ script/app.js
@@ -1,60 +1,59 @@
-const log = console.log;
-const urlData = "https://estivenmayhuay.github.io/Time_Tracking_Dashboard/data.json"
+const urlData =
+  "https://estivenmayhuay.github.io/Time_Tracking_Dashboard/data.json";
 
-$items = Array.from(document.querySelectorAll('.items'));
-$btnTime = Array.from(document.querySelectorAll('.btnTime'));
-$btnList = Array.from(document.querySelectorAll('.btnTime'));
+$items = Array.from(document.querySelectorAll(".items"));
+$btnTime = Array.from(document.querySelectorAll(".btnTime"));
+$btnList = Array.from(document.querySelectorAll(".btnTime"));
 
 // that link is for localhost -> http://127.0.0.1:5500/data.json
 // that link is for url github -> https://estivenmayhuay.github.io/Time_Tracking_Dashboard/data.json
 
-async function obtainData () {
+async function obtainData() {
   const response = await fetch(urlData),
-        json = await response.json();
+    json = await response.json();
 
   return json;
 }
 
-function renderTime (arrTime) {
-  let timesCurr = Array.from(document.querySelectorAll('.timeCurrent')),
-      timesPrev = Array.from(document.querySelectorAll('.timePrevious'));
+function renderTime(arrTime) {
+  let timesCurr = Array.from(document.querySelectorAll(".timeCurrent")),
+    timesPrev = Array.from(document.querySelectorAll(".timePrevious"));
 
-  timesCurr.forEach( (timeCurr, index) => {
+  timesCurr.forEach((timeCurr, index) => {
     timeCurr.innerHTML = `${arrTime[index].current}hrs`;
   });
 
-  timesPrev.forEach( (timePrev, index) => {
+  timesPrev.forEach((timePrev, index) => {
     timePrev.innerHTML = `Last Week - ${arrTime[index].previous}hrs`;
   });
-
 }
 
-async function changeTime (e) {
-  let dataTimeBtn = e.target.getAttribute('data-time'),
-      objJSON = await obtainData(),
-      objectTime = [];
+async function changeTime(e) {
+  let dataTimeBtn = e.target.getAttribute("data-time"),
+    objJSON = await obtainData(),
+    objectTime = [];
 
-  objJSON.forEach( obj => {
-    if(dataTimeBtn == "daily") objectTime.push(obj.timeframes.daily);
-    else if(dataTimeBtn == "weekly") objectTime.push(obj.timeframes.weekly);
-    else if(dataTimeBtn == "monthly") objectTime.push(obj.timeframes.monthly);
+  objJSON.forEach((obj) => {
+    if (dataTimeBtn == "daily") objectTime.push(obj.timeframes.daily);
+    else if (dataTimeBtn == "weekly") objectTime.push(obj.timeframes.weekly);
+    else if (dataTimeBtn == "monthly") objectTime.push(obj.timeframes.monthly);
   });
 
   // show int the DOM The time
   renderTime(objectTime);
 }
 
-$btnTime.forEach(btn => {
-  btn.addEventListener('click', changeTime);
+$btnTime.forEach((btn) => {
+  btn.addEventListener("click", changeTime);
 });
 
-$btnTime.forEach(btn => {
-  btn.addEventListener('click', () => {
+$btnTime.forEach((btn) => {
+  btn.addEventListener("click", () => {
     // reset active buttons
-    document.querySelectorAll('li.active').forEach( listActive => {
-      listActive.classList.remove('active');
+    document.querySelectorAll("li.active").forEach((listActive) => {
+      listActive.classList.remove("active");
     });
 
-    btn.classList.add('active');
+    btn.classList.add("active");
   });
-})
+});
